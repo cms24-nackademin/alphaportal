1fbcb96da26f14783face3541d4e2bb272ac747b
Hans Mattin-Lassei <hans.mattin-lassei@epndata.com>
2025-03-10 09:06:28 +0100

    Lektion 3 (Reboot)

--- WebApp/obj/Debug/net9.0/WebApp.AssemblyInfo.cs
+++ WebApp/obj/Debug/net9.0/WebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c475af215ff921f1637e1987b3ae8c5078e89758")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
